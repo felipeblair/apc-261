3e21adc794644616db4897356edab713229a14eb
Felipe Blair <felipe.teixeira@iesb.edu.br>
2026-03-11 21:57:51 -0300

    adiciona codigos da aula02

---
+++ aulas/aula02/saida.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("estou usando a funcao printf\n");
+
+    printf("Programar em C eh TOP!\n");
+
+    printf("%11i\n", 10+10);
+
+    printf("%.2f\n", 3.1415);
+
+    printf("%s", "isso eh um string\n");
+    return 0;
+}
